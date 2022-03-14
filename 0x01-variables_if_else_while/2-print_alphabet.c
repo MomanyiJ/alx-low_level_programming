a78d08bb2a94f1988ae4764dfd48e3d8ce2f4496
MomanyiJ <momanyij04@gmail.com>
2022-03-14 12:42:31 -0700

    abc in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - reads out alphabet in small letters
+ *
+ *Description: prints small letters alphabet a-z
+ *
+ *return: 0
+ */
+
+
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+		putchar(A);
+	putchar ('\n');
+	return (0);
+
+
+
+}
