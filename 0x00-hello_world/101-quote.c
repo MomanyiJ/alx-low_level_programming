fdf6fe3a0357b64c3fde06268145181da93864a1
MomanyiJ <momanyij04@gmail.com>
2022-03-13 23:21:55 -0700

    corrected header error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+
+
 /**
  *main -Entry point
  *
@@ -7,7 +10,7 @@
 
 int main(void)
 {
-	Write(1,"and that piece of art is useful\" Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 
