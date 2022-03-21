1a90ea84dd18bb4d07fd8fc4acb51d416d6ad101
MomanyiJ <momanyij04@gmail.com>
2022-03-20 22:20:17 -0700

    code printing numbers 0to9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers 0 to 9
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
