de0ce1bb5fc0c3953d0361d8056da5882db22796
MomanyiJ <momanyij04@gmail.com>
2022-03-24 13:59:39 -0700

    repo, README, and first function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - joins two strings together
+ * @dest: where it is going to be joint to
+ * @src: the string to be joint
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, len1 = 0, len2 = 0;
+	/*getting the destination length */
+	while (dest[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	/*getting length of src */
+	while (src[j] != '\0')
+	{
+		len2++;
+		i++;
+	}
+	/* appending source to destination */
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len2 + i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+###More ARRAYs and Pointers
