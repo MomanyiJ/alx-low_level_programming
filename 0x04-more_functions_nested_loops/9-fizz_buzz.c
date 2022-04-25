a773abb6fff37aeeff67c71b0d0613a71ec6cea9
MomanyiJ <momanyij04@gmail.com>
2022-04-25 04:10:52 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - checks fizz buzz and either
+ * Return: 0 on success and 1 for a fault
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else if (i == 1)
+			printf("%d", i);
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+	return (0);
+}
