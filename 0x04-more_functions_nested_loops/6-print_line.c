1f056230e340c2496477a933429d1198dc2478e1
MomanyiJ <momanyij04@gmail.com>
2022-04-25 02:33:43 -0700

    function drawing straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Prints_
+ * Return: Does not return any value
+ * @n: Number of _ to be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
