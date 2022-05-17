a773a5edd2a0d3da1f26d10d0b5380abf540323e
MomanyiJ <momanyij04@gmail.com>
2022-05-17 08:02:13 -0700

    extracredit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	/*initialize first and second terms*/
+	long int t1 = 1;
+	long int t2 = 2;
+
+	/* initialize the next term (3rd term)*/
+	long int nextTerm = t1 + t2;
+
+	/* print the first two terms t1 and t2*/
+	printf("%ld, %ld", t1, t2);
+
+	/* print 3rd to 50th terms*/
+	for (i = 0; i < 48; ++i)
+	{
+		nextTerm = t1 + t2;
+		printf(", %ld", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("\n");
+	return (0);
+}
